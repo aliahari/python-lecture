d604f733f6fcdb8c2754c1611745b7a8591e8a61
Ali Ahari <ahari@fzi.de>
2019-11-04 02:35:35 +0100

    added week4

---
+++ Week3/xgram-1.py
@@ -0,0 +1,13 @@
+users = ["Alice", "Bob"]
+postsAlice = ["Wow! Iam finally in Karlsruhe!", "Great music @ AKK"]
+postsBob = ["Great time at python class!"]
+imgsAlice = ["shorturl.at/chDUX", "shorturl.at/aorP1"]
+imgsBob =["shorturl.at/wxFLR"]
+postsBob.append("I just learned about lists!") #add a new post 
+oldPostsAlice = ["Got the ticket", "On my way to Karlsruhe"]
+postsAlice = oldPostsAlice + postsAlice #add oldPostsAlice to the beginning of postsAlice
+print(postsAlice[-2:]) #print last two posts
+ 
+
+
+
